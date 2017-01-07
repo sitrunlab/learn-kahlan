a87bf92838091b4f7abce8727e0ca1c2da06fa7b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2017-01-07 15:18:47 +0700

    add expect()->toBeCalled()->once() for time()

--- spec/UsageFunctionsSpec.php
+++ spec/UsageFunctionsSpec.php
@@ -5,19 +5,20 @@ namespace App\Spec;
 use App\UsageFunctions;
 
 describe('UsageFunctions', function () {
-    
+
     describe('getCurrentTime()', function () {
-        
+
         it('returns expected current time', function () {
 
             $expected = 1477897159;
             allow('time')->toBeCalled()->andReturn($expected);
-            
+            expect('time')->toBeCalled()->once();
+
             $actual = (new UsageFunctions())->getCurrentTime();
             expect($actual)->toBe($expected);
-            
+
         });
-        
+
     });
 
 });
