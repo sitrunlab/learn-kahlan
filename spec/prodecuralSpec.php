403a193ca54caae828918f70e6f3e5b470614534
Josh Habdas <jhabdas@gmail.com>
2017-05-19 22:24:34 +0800

    feat(examples): add procedural example
    uses procedural programming and namespaces for encapsulation with the use of higher order functions
    to provide an example of how to achieve a functional programming style with PHP. uses void return
    statements added in php 7.1 to signal to the compiler when a side effect occurs within a function.

---
+++ spec/prodecuralSpec.php
@@ -0,0 +1,13 @@
+<?php
+namespace procedural\spec;
+
+use function prodecural\echoScript;
+
+describe('procedural', function () {
+  describe('->echoScript()', function () {
+    it('echos input string inside a script tag', function () {
+      $closure = function() { echoScript('baz'); };
+      expect($closure)->toEcho('<script>baz</script>');
+    });
+  });
+});
