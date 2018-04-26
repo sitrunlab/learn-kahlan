73a42308119ace8caf7700f9e3c9cda83aa2845d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-04-26 21:32:11 +0700

    update to kahlan ^4.1.3

--- spec/UsageFunctionsSpec.php
+++ spec/UsageFunctionsSpec.php
@@ -12,6 +12,7 @@ describe('UsageFunctions', function () {
 
             $expected = 1477897159;
             allow('time')->toBeCalled()->andReturn($expected);
+            expect('time')->toBeCalled()->once();
 
             $actual = (new UsageFunctions())->getCurrentTime();
             expect($actual)->toBe($expected);
